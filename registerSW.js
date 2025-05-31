7355b64254c659836d227be0cf275438dd2e1df3
jlsanzbreton <147775626+jlsanzbreton@users.noreply.github.com>
2025-05-31 19:19:20 +0000

    Deploying to gh-pages from @ jlsanzbreton/smart-receipt-analyzer2@fe147ac60def2c3ea8e88edf06f80a5bba497882 🚀

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/smart-receipt-analyzer2/sw.js', { scope: '/smart-receipt-analyzer2/' })})}
